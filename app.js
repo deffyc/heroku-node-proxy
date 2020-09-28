918ca9acc104d0ce089bd6f3f281fba6c7f54051
deffyc <deffyc@users.noreply.github.com>
2020-09-28 22:51:55 +0800

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,39 @@ var unblocker = require('./lib/unblocker.js');
 var Transform = require('stream').Transform;
 
 var app = express();
+// Authenticator
+app.use(function(req, res, next) {
+    var auth;
 
+    // check whether an autorization header was send    
+    if (req.headers.authorization) {
+      // only accepting basic auth, so:
+      // * cut the starting "Basic " from the header
+      // * decode the base64 encoded username:password
+      // * split the string at the colon
+      // -> should result in an array
+      auth = new Buffer(req.headers.authorization.substring(6), 'base64').toString().split(':');
+      // use Buffer.from in with node v5.10.0+ 
+      // auth = Buffer.from(req.headers.authorization.substring(6), 'base64').toString().split(':');
+    }
+
+    // checks if:
+    // * auth array exists 
+    // * first value matches the expected user 
+    // * second value the expected password
+    if (!auth || auth[0] !== process.env["OAUTH_CLIENT_ID"] || auth[1] !== process.env["OAUTH_CLIENT_SECRET"]) {
+        // any of the tests failed
+        // send an Basic Auth request (HTTP Code: 401 Unauthorized)
+        res.statusCode = 401;
+        // MyRealmName can be changed to anything, will be prompted to the user
+        res.setHeader('WWW-Authenticate', 'Basic realm="MyRealmName"');
+        // this will displayed in the browser when authorization is cancelled
+        res.end('Unauthorized');
+    } else {
+        // continue with processing, user was authenticated
+        next();
+    }
+});
 var google_analytics_id = process.env.GA_ID || null;
 
 function addGa(html) {
@@ -66,39 +98,6 @@ app.use(unblocker(unblockerConfig));
 // serve up static files *after* the proxy is run
 app.use('/', express.static(__dirname + '/public'));
 
-// Authenticator
-app.use(function(req, res, next) {
-    var auth;
-
-    // check whether an autorization header was send    
-    if (req.headers.authorization) {
-      // only accepting basic auth, so:
-      // * cut the starting "Basic " from the header
-      // * decode the base64 encoded username:password
-      // * split the string at the colon
-      // -> should result in an array
-      auth = new Buffer(req.headers.authorization.substring(6), 'base64').toString().split(':');
-      // use Buffer.from in with node v5.10.0+ 
-      // auth = Buffer.from(req.headers.authorization.substring(6), 'base64').toString().split(':');
-    }
-
-    // checks if:
-    // * auth array exists 
-    // * first value matches the expected user 
-    // * second value the expected password
-    if (!auth || auth[0] !== process.env["OAUTH_CLIENT_ID"] || auth[1] !== process.env["OAUTH_CLIENT_SECRET"]) {
-        // any of the tests failed
-        // send an Basic Auth request (HTTP Code: 401 Unauthorized)
-        res.statusCode = 401;
-        // MyRealmName can be changed to anything, will be prompted to the user
-        res.setHeader('WWW-Authenticate', 'Basic realm="MyRealmName"');
-        // this will displayed in the browser when authorization is cancelled
-        res.end('Unauthorized');
-    } else {
-        // continue with processing, user was authenticated
-        next();
-    }
-});
 
 // this is for users who's form actually submitted due to JS being disabled or whatever
 app.get("/no-js", function(req, res) {
